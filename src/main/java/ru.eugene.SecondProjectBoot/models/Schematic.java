20b923468ab26b201acf1ce14ee471722915796a
Eugene <74543647+teoretik-eugene@users.noreply.github.com>
2023-02-27 21:03:37 +0300

    Create Schematic.java

---
+++ src/main/java/ru.eugene.SecondProjectBoot/models/Schematic.java
@@ -0,0 +1,9 @@
+package ru.eugene.SecondProjectBoot.models;
+
+import java.util.List;
+
+public class Schematic extends Subject{
+    public Schematic(String subjectName, List<String> literature) {
+        super(subjectName, literature);
+    }
+}
