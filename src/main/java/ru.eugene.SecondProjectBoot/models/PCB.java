a0eee3cf3c7d6a92fdd82df31160229b644c9b5b
Eugene <74543647+teoretik-eugene@users.noreply.github.com>
2023-02-27 21:03:13 +0300

    Create PCB.java

---
+++ src/main/java/ru.eugene.SecondProjectBoot/models/PCB.java
@@ -0,0 +1,9 @@
+package ru.eugene.SecondProjectBoot.models;
+
+import java.util.List;
+
+public class PCB extends Schematic{
+    public PCB(String subjectName, List<String> literature) {
+        super(subjectName, literature);
+    }
+}
