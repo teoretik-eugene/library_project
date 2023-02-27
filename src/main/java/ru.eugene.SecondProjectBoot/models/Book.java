2bfc29a8a09787a99d1a03c362eaba1615221b8a
Eugene <74543647+teoretik-eugene@users.noreply.github.com>
2023-02-27 21:02:58 +0300

    Create Book.java

---
+++ src/main/java/ru.eugene.SecondProjectBoot/models/Book.java
@@ -0,0 +1,103 @@
+package ru.eugene.SecondProjectBoot.models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "author")
+    private String author;
+    @Column(name = "year")
+    private int year;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person person;
+
+    @Column(name = "assigned_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date assignedAt;
+
+    @Transient
+    private boolean isOverdue;
+
+    public Book(String name, String author, int year, int id) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+        this.id = id;
+    }
+
+    public Book(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Date getAssignedAt() {
+        return assignedAt;
+    }
+
+    public void setAssignedAt(Date assignedAt) {
+        this.assignedAt = assignedAt;
+    }
+
+    public boolean isOverdue() {
+        return isOverdue;
+    }
+
+    public void setOverdue(boolean overdue) {
+        isOverdue = overdue;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " - " + this.name + ", " + this.author + ", " + this.year + " : " + this.person;
+    }
+}
